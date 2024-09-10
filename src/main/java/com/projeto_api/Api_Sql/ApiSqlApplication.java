a277d48d8ca27ffebf02bec52e933bfec6be33b9
Bruno Souza <brunocloud78@gmail.com>
2024-09-10 20:18:55 -0300

    Projeto Api

---
+++ src/main/java/com/projeto_api/Api_Sql/ApiSqlApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_api.Api_Sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_api/Api_Sql/ApiSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_api.Api_Sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
